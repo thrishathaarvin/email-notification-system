03f331e7f910ce7acc1cf04cf4fd0095eddf80f4
Thrishatha <thrishatha.arvin@growfin.ai>
2026-02-22 12:16:13 +0530

    update consumer

--- backend/src/main/java/com/example/email_service/messaging/SendGridEventConsumer.java
+++ backend/src/main/java/com/example/email_service/messaging/SendGridEventConsumer.java
@@ -7,40 +7,41 @@ import com.example.email_service.model.entity.EmailNotification;
 import com.example.email_service.model.enums.DeliveryStatus;
 import com.example.email_service.repository.DeliveryHistoryRepository;
 import com.example.email_service.repository.EmailRepository;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
+//This represents a message consumer which process sendgrid events and populate delivery history table
 @Component
 public class SendGridEventConsumer {
 
+    //Constructor injection
     private final EmailRepository emailRepository;
     private final DeliveryHistoryRepository historyRepository;
-
     public SendGridEventConsumer(EmailRepository emailRepository,
                                  DeliveryHistoryRepository historyRepository) {
         this.emailRepository = emailRepository;
         this.historyRepository = historyRepository;
     }
 
+    //this gets triggered when a new sendgrid event arrives in queue
     @RabbitListener(queues = RabbitMQConfig.EMAIL_STATUS_QUEUE)
     public void consume(SendGridEventDto event) {
 
+        //Mapping sendgrid event to an internal email record
         EmailNotification email = emailRepository
                 .findByProviderTrackingId(event.getSg_message_id())
                 .orElseThrow(() -> new RuntimeException(
                         "Email not found for SendGrid ID: " + event.getSg_message_id()
                 ));
 
+        //converting string event to enum, updates and saves in repo
         DeliveryStatus status = mapEvent(event.getEvent());
         email.setDeliveryStatus(status);
         emailRepository.save(email);
 
+        //now, this is created in delivery history table for audit purpose
         EmailDeliveryHistory history = new EmailDeliveryHistory();
         history.setEmailId(email.getId());
         history.setStatus(status);
@@ -50,6 +51,7 @@ public class SendGridEventConsumer {
         historyRepository.save(history);
     }
 
+    //Conversion of string to enum logic
     private DeliveryStatus mapEvent(String event) {
         return switch (event.toLowerCase()) {
             case "sent", "delivered" -> DeliveryStatus.SENT;
@@ -59,6 +61,7 @@ public class SendGridEventConsumer {
         };
     }
 
+    //converts dto to JSON for storage reasons
     private String toJson(SendGridEventDto event) {
         try {
             return new com.fasterxml.jackson.databind.ObjectMapper()
