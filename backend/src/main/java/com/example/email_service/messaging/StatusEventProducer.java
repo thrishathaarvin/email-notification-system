065df8681df8ee94a25393c49b208245b7d50a2c
Thrishatha <thrishatha.arvin@growfin.ai>
2026-02-22 12:20:27 +0530

    update producer

--- backend/src/main/java/com/example/email_service/messaging/StatusEventProducer.java
+++ backend/src/main/java/com/example/email_service/messaging/StatusEventProducer.java
@@ -5,15 +5,17 @@ import com.example.email_service.dto.SendGridEventDto;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+//This publishes the sendgrid events inside the queue
 @Component
 public class StatusEventProducer {
 
+    //Constructor injection
     private final RabbitTemplate rabbitTemplate;
-
     public StatusEventProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
+    //event is converted to JSON, then sent to queue, from which consumers use
     public void publishStatus(SendGridEventDto event) {
         rabbitTemplate.convertAndSend(RabbitMQConfig.EMAIL_STATUS_QUEUE, event);
     }
