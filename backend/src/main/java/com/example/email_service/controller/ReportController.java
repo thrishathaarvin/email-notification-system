47ef43af356eae8b1bdec695daec7e773cb67922
Thrishatha <thrishatha.arvin@growfin.ai>
2026-02-22 09:35:19 +0530

    update report controller

--- backend/src/main/java/com/example/email_service/controller/ReportController.java
+++ backend/src/main/java/com/example/email_service/controller/ReportController.java
@@ -4,27 +4,24 @@ import com.example.email_service.dto.ReportResponseDto;
 import com.example.email_service.service.ReportService;
 import org.springframework.web.bind.annotation.*;
 
+//This exposes API for read only reports and dashboard
 @RestController
 @RequestMapping("/api/reports")
 public class ReportController {
 
+    //Constructor injection
     private final ReportService service;
-
     public ReportController(ReportService service) {
         this.service = service;
     }
 
-    /**
-     * Simple summary endpoint
-     */
+    //Returns sent and failed email counts
     @GetMapping("/summary")
     public ReportResponseDto getSummary() {
         return service.getSummary();
     }
 
-    /**
-     * Dashboard endpoint (for charts)
-     */
+    //Visualization purposes
     @GetMapping("/dashboard")
     public ReportResponseDto getDashboardData() {
         return service.getDashboardData();
