02f4aa685c0579bec67a6c3c5badd6749c083bbd
Thrishatha <thrishatha.arvin@growfin.ai>
2026-02-22 10:41:10 +0530

    update Webhook controller

--- backend/src/main/java/com/example/email_service/controller/WebhookController.java
+++ backend/src/main/java/com/example/email_service/controller/WebhookController.java
@@ -7,21 +7,23 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//this receives sendgrid events and forward it to queue, which is now not done due to being locally hosted
 @RestController
 @RequestMapping("/api/webhooks/sendgrid")
 public class WebhookController {
 
+    //Constructor injection
     private final StatusEventProducer statusProducer;
-
     public WebhookController(StatusEventProducer statusProducer) {
         this.statusProducer = statusProducer;
     }
 
+    //fetches status and pushes to status queue
     @PostMapping
     public ResponseEntity<Void> receiveEvents(@RequestBody List<SendGridEventDto> events) {
 
         for (SendGridEventDto event : events) {
-            statusProducer.publishStatus(event); // sends to EMAIL_STATUS_QUEUE
+            statusProducer.publishStatus(event);
         }
 
         return ResponseEntity.ok().build();
