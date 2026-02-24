41236b02d6f5fa264d948ccbd96822646eb873c9
Thrishatha <thrishatha.arvin@growfin.ai>
2026-02-24 11:55:50 +0530

    update email service

--- backend/src/main/java/com/example/email_service/service/EmailDispatchService.java
+++ backend/src/main/java/com/example/email_service/service/EmailDispatchService.java
@@ -1,8 +1,5 @@
 package com.example.email_service.service;
 
-import com.example.email_service.dto.EmailQueueMessage;
-import com.example.email_service.model.entity.EmailNotification;
-import com.example.email_service.model.enums.DeliveryStatus;
 import com.example.email_service.repository.EmailRepository;
 import com.sendgrid.*;
 import com.sendgrid.helpers.mail.Mail;
@@ -18,57 +15,8 @@ import java.io.IOException;
 @Service
 public class EmailDispatchService {
 
-
     @Value("${sendgrid.api.key}")
     private String sendGridApiKey;
-
-    //Dependency injection
-    private final SendGrid sendGrid;
-    private final EmailRepository emailRepository;
-    public EmailDispatchService(SendGrid sendGrid, EmailRepository emailRepository) {
-        this.sendGrid = sendGrid;
-        this.emailRepository = emailRepository;
-    }
-
-    public void sendEmail(EmailQueueMessage message) {
-
-        //loads the email entity
-        EmailNotification email = emailRepository
-                .findById(message.getEmailId())
-                .orElseThrow(() -> new RuntimeException("Email not found"));
-
-        // builds sendgrid mail object with dto data
-        Email fromEmail = new Email(message.getFrom());  // <--- dynamic sender
-        Email toEmail = new Email(message.getTo());
-        Content content = new Content("text/html", message.getBody());
-        Mail mail = new Mail(fromEmail, message.getSubject(), toEmail, content);
-
-        //sends mail, if success SENT else FAILED
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-
-            Response response = sendGrid.api(request);
-
-            if (response.getStatusCode() == 202) {
-                email.setStatus(DeliveryStatus.SENT);
-
-
-                String messageId = response.getHeaders().get("X-Message-Id");
-                email.setProviderTrackingId(messageId);
-
-            } else {
-                email.setStatus(DeliveryStatus.FAILED);
-            }
-
-        } catch (Exception e) {
-            email.setStatus(DeliveryStatus.FAILED);
-        }
-
-        emailRepository.save(email);
-    }
 
     public String sendEmailDirectly(String from, String to, String subject, String body) throws IOException, IOException {
         Email fromEmail = new Email(from);
